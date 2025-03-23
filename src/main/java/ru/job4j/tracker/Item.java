12ca421c2f7e1bbe2a684ef2f947f81f3dd621ef
m0neV <sadihsalahov7@gmail.com>
2025-03-23 19:42:43 +0500

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,13 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Item {
     private int id;
     private String name;
+    LocalDateTime created = LocalDateTime.now();
 
     public Item() {
     }
@@ -30,5 +35,9 @@ public class Item {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 }
