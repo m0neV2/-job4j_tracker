d6be5f1e236877a6ac60e6511f7f5d25f995a7dc
m0neV <sadihsalahov7@gmail.com>
2025-04-16 18:28:06 +0500

    9.1. Тестирование. Подготовка данных. [#182960]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -14,4 +14,53 @@ class StartUITest {
         Item expected = new Item("Fix PC");
         assertThat(created.getName()).isEqualTo(expected.getName());
     }
+
+    @Test
+    void whenReplaceItemThenSuccess() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Original Item");
+        tracker.add(item);
+        String[] answers = {String.valueOf(item.getId()), "Updated Item"};
+        StartUI.replaceItem(new MockInput(answers), tracker);
+        Item updated = tracker.findById(item.getId());
+        assertThat(updated.getName()).isEqualTo("Updated Item");
+    }
+
+    @Test
+    void whenReplaceItemWithInvalidIdThenFail() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Original Item");
+        tracker.add(item);
+
+        String[] answers = {"999", "Updated Item"};
+
+        StartUI.replaceItem(new MockInput(answers), tracker);
+
+        Item notUpdated = tracker.findById(item.getId());
+        assertThat(notUpdated.getName()).isEqualTo("Original Item");
+    }
+
+    @Test
+    void whenDeleteExistingItemThenSuccess() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("Test Item");
+        tracker.add(item);
+
+        Input input = new MockInput(new String[]{String.valueOf(item.getId())});
+
+        StartUI.deleteItem(input, tracker);
+
+        assertThat(tracker.findById(item.getId())).isNull();
+    }
+
+    @Test
+    void whenDeleteNonExistingItemThenFail() {
+        Tracker tracker = new Tracker();
+
+        Input input = new MockInput(new String[]{"999"});
+
+        StartUI.deleteItem(input, tracker);
+
+        assertThat(tracker.findAll()).isEmpty();
+    }
 }
