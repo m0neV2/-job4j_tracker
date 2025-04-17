71b0f76af624cef6429fe99b2a0c68aeb11f069e
m0neV <sadihsalahov7@gmail.com>
2025-04-17 21:42:31 +0500

    12.0. Написать тесты на StartUI. [#785]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -5,59 +5,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class StartUITest {
     @Test
-    void whenAddItem() {
-        String[] answers = {"Fix PC"};
-        Input input = new MockInput(answers);
+    void whenCreateItem() {
+        Input input = new MockInput(
+                new String[] {"0", "Item name", "1"}
+        );
         Tracker tracker = new Tracker();
-        StartUI.createItem(input, tracker);
-        Item created = tracker.findAll()[0];
-        Item expected = new Item("Fix PC");
-        assertThat(created.getName()).isEqualTo(expected.getName());
+        UserAction[] actions = {
+                new CreateAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findAll()[0].getName()).isEqualTo("Item name");
     }
 
     @Test
-    void whenReplaceItemThenSuccess() {
+    void whenReplaceItem() {
         Tracker tracker = new Tracker();
-        Item item = new Item("Original Item");
-        tracker.add(item);
-        String[] answers = {String.valueOf(item.getId()), "Updated Item"};
-        StartUI.replaceItem(new MockInput(answers), tracker);
-        Item updated = tracker.findById(item.getId());
-        assertThat(updated.getName()).isEqualTo("Updated Item");
+        Item item = tracker.add(new Item("Replaced item"));
+        String replacedName = "New item name";
+        Input input = new MockInput(
+                new String[] {"0", String.valueOf(item.getId()), replacedName, "1"}
+        );
+        UserAction[] actions = {
+                new ReplaceAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
+        assertThat(tracker.findById(item.getId()).getName()).isEqualTo(replacedName);
     }
 
     @Test
-    void whenReplaceItemWithInvalidIdThenFail() {
+    void whenDeleteItem() {
         Tracker tracker = new Tracker();
-        Item item = new Item("Original Item");
-        tracker.add(item);
-
-        String[] answers = {"999", "Updated Item"};
-
-        StartUI.replaceItem(new MockInput(answers), tracker);
-
-        Item notUpdated = tracker.findById(item.getId());
-        assertThat(notUpdated.getName()).isEqualTo("Original Item");
-    }
-
-    @Test
-    void whenDeleteExistingItemThenSuccess() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("Test Item");
-        tracker.add(item);
-
-        Input input = new MockInput(new String[]{String.valueOf(item.getId())});
-
-        StartUI.deleteItem(input, tracker);
-
+        Item item = tracker.add(new Item("Deleted item"));
+        Input input = new MockInput(
+                new String[] {"0", String.valueOf(item.getId()), "1"}
+        );
+        UserAction[] actions = {
+                new DeleteAction(),
+                new ExitAction()
+        };
+        new StartUI().init(input, tracker, actions);
         assertThat(tracker.findById(item.getId())).isNull();
-    }
-
-    @Test
-    void whenDeleteNonExistingItemThenFail() {
-        Tracker tracker = new Tracker();
-        Input input = new MockInput(new String[]{"999"});
-        StartUI.deleteItem(input, tracker);
-        assertThat(tracker.findAll()).isEmpty();
     }
 }
