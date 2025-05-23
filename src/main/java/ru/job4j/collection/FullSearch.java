9a4ec51ee5fabbbbb72db2639f3797b0ff6b6e39
m0neV <sadihsalahov7@gmail.com>
2025-05-23 22:01:54 +0500

    1. Уникальные задачи [#212653]

---
+++ src/main/java/ru/job4j/collection/FullSearch.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class FullSearch {
+
+    public Set<String> extractNumber(List<Task> tasks) {
+        HashSet<String> unique = new HashSet<>();
+        for (Task num : tasks) {
+        unique.add(num.getNumber());
+        }
+        return unique;
+    }
+}
