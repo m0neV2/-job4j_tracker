62c900d70dc9cb42544e18f32106702031e6a8af
m0neV <sadihsalahov7@gmail.com>
2025-05-31 20:55:10 +0500

    3. Банковские переводы [#10038 #544999]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -16,29 +16,23 @@ public class BankService {
     }
 
     public void deleteUser(String passport) {
-        if (passport != null) {
-            User user = findByPassport(passport);
-            if (user != null) {
-                users.remove(user);
-            }
-        }
+        users.remove(new User(passport, ""));
     }
 
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
-        List<Account> accounts = users.get(user);
-        if (user == null) {
-            return;
-        }
+        if (user != null) {
+            List<Account> accounts = users.get(user);
             if (!accounts.contains(account)) {
                 accounts.add(account);
             }
         }
+    }
 
     public User findByPassport(String passport) {
         for (User user : users.keySet()) {
             if (passport.equals(user.getPassport())) {
-             return user;
+                return user;
             }
         }
         return null;
@@ -62,29 +56,24 @@ public class BankService {
                                  String destinationPassport, String destinationRequisite,
                                  double amount) {
         boolean result = false;
-        if (sourcePassport == null || sourceRequisite == null
-                || destinationPassport == null || destinationRequisite == null
-        || amount <= 0) {
-            return result;
+        if (sourcePassport != null && sourceRequisite != null
+                && destinationPassport != null && destinationRequisite != null
+                && amount > 0) {
+
+            Account sourceAccount = findByRequisite(sourcePassport, sourceRequisite);
+            Account destAccount = findByRequisite(destinationPassport, destinationRequisite);
+
+            if (sourceAccount != null && destAccount != null
+                    && sourceAccount.getBalance() >= amount) {
+                sourceAccount.setBalance(sourceAccount.getBalance() - amount);
+                destAccount.setBalance(destAccount.getBalance() + amount);
+                result = true;
+            }
         }
-        Account sourceaccount = findByRequisite(sourcePassport, sourceRequisite);
-        if (sourceaccount == null) {
-            return result;
-        }
-        Account destaccount = findByRequisite(destinationPassport, destinationRequisite);
-        if (destaccount == null) {
-            return result;
-        }
-        if (sourceaccount.getBalance() < amount) {
-            return result;
-        }
-        sourceaccount.setBalance(sourceaccount.getBalance() - amount);
-        destaccount.setBalance(destaccount.getBalance() + amount);
-        result = true;
         return result;
     }
 
-    public List<Account> getAccounts(User user) {
-        return users.get(user);
+        public List<Account> getAccounts(User user) {
+            return users.get(user);
+        }
     }
-}
